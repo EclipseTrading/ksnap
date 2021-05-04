89a49c877a51141feec9d17f7a446b4b894cde30
Alex Fung <afung@eclipseoptions.com>
2021-05-04 12:00:08 +0800

    Refactored to use concurrent futures and added unit test.

--- ksnap/__main__.py
+++ ksnap/__main__.py
@@ -5,10 +5,12 @@ import sys
 from ksnap.config import KsnapConfig
 from ksnap.manager import KsnapManager
 
+logger = logging.getLogger(__name__)
+
 
 def set_up_logger():
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter(
         '%(asctime)-15s %(levelname)-8s %(message)s'))
@@ -19,6 +21,7 @@ def set_up_logger():
 def main():
     set_up_logger()
     config = KsnapConfig.from_cli_args()
+    logger.info(f'Config: {config}')
     ksnap_manager = KsnapManager(config)
     ksnap_manager.run()
     return
